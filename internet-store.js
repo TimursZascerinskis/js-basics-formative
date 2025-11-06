025019a9d199b2bd6de3e55688eb782f814611be
unknown <your@email.example>
2025-11-06 10:54:09 +0200

    5.uzd

---
+++ internet-store.js
@@ -0,0 +1,6 @@
+const veikals = [
+    {nosaukums: "dators", kategorija:"Elektronika", cena:1200, daudzums:4},
+    {nosaukums: "cepure", kategorija:"Apģērbi", cena:12, daudzums:100},
+    {nosaukums: "telefons", kategorija:"Elektronika", cena:120, daudzums:40},
+
+]
