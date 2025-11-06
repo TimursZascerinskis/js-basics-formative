d52f737e16b6b5f8ccc54284ce57eb266b6b9d43
unknown <your@email.example>
2025-11-06 10:46:34 +0200

    3.uzd

---
+++ people-array.js
@@ -0,0 +1,10 @@
+const people = [
+    {name:"Timur", age:17, isStudent:true},
+    {name:"Jarik", age:12, isStudent:false}
+]
+
+
+
+for (const element of people) {
+  console.log("Vārds: ",element.name);
+}
