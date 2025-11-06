3baef873e7eb90dfa82df415c705517496ca2dfb
unknown <your@email.example>
2025-11-06 10:28:51 +0200

    2.uzd

---
+++ ticket-price.js
@@ -0,0 +1,12 @@
+let visitorAge = 12;
+let isStudent = true;
+
+if (visitorAge<=6){
+    console.log("ieeja ir bez maksas.")
+}
+else if(isStudent ===true){
+    console.log("biļetes cena ir 5 EUR.")
+}
+else {
+    console.log("Visiem pārējiem biļetes cena ir 10 EUR.")
+}
